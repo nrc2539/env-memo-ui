fac523ea523e70550b776b491d9bf1c13eea1d23
nrc-dev <narongchai.b2539@gmail.com>
2026-05-18 18:00:24 +0700

    fix: update validation in login page

--- src/features/auth/pages/LoginPage/LoginPage.tsx
+++ src/features/auth/pages/LoginPage/LoginPage.tsx
@@ -12,9 +12,7 @@ const validationSchema = Yup.object({
   email: Yup.string()
     .email("Invalid email address")
     .required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
+  password: Yup.string().required("Password is required"),
 });
 
 export default function LoginPage({ initialValues, onSubmit }: LoginPageProps) {
